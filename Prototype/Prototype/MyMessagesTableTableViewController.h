c09ad8afb5b0ddf37b9321614d5870b343291f45
deroliver <Derik@Deriks-MacBook-Pro.local>
2016-03-15 11:08:20 -0700

    Fixed some Constraints

---
+++ Prototype/Prototype/MyMessagesTableTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyMessagesTableTableViewController.h
+//  Prototype
+//
+//  Created by Derik Oliver on 3/14/16.
+//  Copyright © 2016 Derik Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyMessagesTableTableViewController : UITableViewController
+
+@end
